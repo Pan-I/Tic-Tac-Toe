796cb7c20682acefec597895484f7c4029d4fe70
Ian Pommer <9175791+Pan-I@users.noreply.github.com>
2024-11-24 12:47:50 -0800

    In-Code Commenting (#16) (#17)
    Adding and revising comments for clarity.

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -1,31 +1,22 @@
 using Godot;
 
-namespace TicTacToe;
+namespace TicTacToe.scripts;
 
 public partial class MainMenu : CanvasLayer
 {
 	[Signal] public delegate void OnePlayerGameEventHandler();
-
 	[Signal] public delegate void TwoPlayerGameEventHandler();
 	[Signal] public delegate void QuitEventHandler();
-
-	public override void _Ready()
-	{
-		//var test = GetNode<Panel>("MainMenuPanel").GetNode<Button>("1PlayerButton");
-		//test.Disabled = true;
-	}
-	
 	private void _on_player_button_pressed()
 	{
-		EmitSignal(SignalName.OnePlayerGame);
+		EmitSignal(TicTacToe.scripts.MainMenu.SignalName.OnePlayerGame);
 	}
 	private void _on_2player_button_pressed()
 	{
-		EmitSignal(SignalName.TwoPlayerGame);
+		EmitSignal(TicTacToe.scripts.MainMenu.SignalName.TwoPlayerGame);
 	}
 	private void _on_quit_button_pressed()
 	{
-		EmitSignal(SignalName.Quit);
+		EmitSignal(TicTacToe.scripts.MainMenu.SignalName.Quit);
 	}
-
 }
